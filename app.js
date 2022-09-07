1420ffdd9f1fb0ce4a14664f4dd65e8e61fb75d4
Your Name <abdulloxortiqov7gmail.com>
2022-09-07 16:36:47 +0500

    typed.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var typed2 = new Typed('#typed2', {
+    strings: ['Я люблю тебя ',
+              'I LOVE YOU',
+              'JE VOUS AIME'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    fadeOut: true,
+    loop: true
+});
